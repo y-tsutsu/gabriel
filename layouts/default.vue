9b0fe186c0a5feebe373919aaecf07c773ae4df1
tsutsu <tsutsu0928@gmail.com>
2020-10-20 15:59:27 +0900

    レイアウトを更新

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,48 @@
 <template>
   <div>
-    <Nuxt />
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="/">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            heigh="28"
+          />
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <nuxt-link class="navbar-item" to="/">ホーム</nuxt-link>
+        </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <!-- ここにログインボタンを⼊れる-->
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="main-body">
+      <!-- これがページのレンダリング要素-->
+      <nuxt />
+    </div>
+    <footer class="footer">
+      <div>
+        <div class="content has-text-centered">
+          <p>&copy; qaSample</p>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
 <style>
 html {
-  font-family:
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
